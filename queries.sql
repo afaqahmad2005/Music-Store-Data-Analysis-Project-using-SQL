931f741fdcd57f109cb48759c8405329ecb99ac1
afaqahmad2005 <afaqshewa1234@gmail.com>
2026-02-24 23:47:52 +0500

    Done

--- queries.sql
+++ queries.sql
@@ -13,3 +13,11 @@ ORDER BY C DESC
 SELECT total FROM invoice
 ORDER BY total DESC
 LIMIT 3
+
+-- Q4: Which city has the best customers? We would like to throw a promotional Music Festivals in the city 
+-- we made the most money. Write a query that returns one city that has the highest sum of invoice totals.
+-- Return both the city name & sum of all invoice totals.
+SELECT SUM(total) AS invoice_total, billing_city
+FROM invoice
+GROUP BY billing_city
+ORDER BY invoice_total DESC
