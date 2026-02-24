e561a2d40fbdd41a49ef8fc34a66f52ddbfe1284
afaqahmad2005 <afaqshewa1234@gmail.com>
2026-02-25 00:13:04 +0500

    Done

--- queries.sql
+++ queries.sql
@@ -30,3 +30,17 @@ JOIN invoice ON customer.customer_id = invoice.customer_id
 GROUP BY customer.customer_id
 ORDER BY total DESC
 LIMIT 1
+
+-- Q6: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+-- Return your list ordered alphabetically by email starting with A
+SELECT DISTINCT email, first_name, last_name
+FROM customer
+JOIN invoice ON customer.customer_id = invoice.customer_id
+JOIN invoice_line ON invoice.invoice_id = invoice_line.invoice_id
+WHERE track_id IN (
+    SELECT track_id 
+    FROM track
+    JOIN genre ON track.genre_id = genre.genre_id
+    WHERE genre.name = 'Rock'
+)
+ORDER BY email;
