d6d0ea5e5b3f0f7f66a848575476458b30d79bb2
afaqahmad2005 <afaqshewa1234@gmail.com>
2026-02-25 23:43:01 +0500

    DONE

--- queries.sql
+++ queries.sql
@@ -56,3 +56,14 @@ WHERE genre.name LIKE 'Rock'
 GROUP BY artist.artist_id
 ORDER BY number_of_songs DESC
 LIMIT 10;
+
+
+-- Q8: Q3: Return all the track names that have a song length longer than the average song length. Return 
+-- the Name and Milliseconds for each track. Order by the song length with the longest songs listed first.
+SELECT name, milliseconds
+FROM track
+WHERE milliseconds >(
+	SELECT AVG(milliseconds) AS avg_track_lenght
+	FROM track
+)
+ORDER BY milliseconds DESC;
